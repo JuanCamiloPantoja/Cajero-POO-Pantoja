93fad4448cd2157c7b8237481b6497f7527dcc2d
Ash20 <Ash20@CAMILO>
2024-08-01 21:36:53 -0500

    Se realiza el commit de inicio de sesión

---
+++ InicioSesion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InicioSesionCamilx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sea bienvenido al mejor cajero automático");
+            if (InicioSesion())
+            {
+                Console.WriteLine("Su inicio de sesión fue un éxito. ");
+                
+            }
+            else
+            {
+                Console.WriteLine("Su inicio de sesión fue un fracaso. ");
+            }
+        }
+
+        static bool InicioSesion()
+        {
+            Console.Write("Por favor, digite su usuario: ");
+            string usuario = Console.ReadLine();
+            Console.Write("Inserte su clave: ");
+            string clave = Console.ReadLine();
+            return usuario == "Camile" && clave == "1033"; 
+        }
+    }
+}
