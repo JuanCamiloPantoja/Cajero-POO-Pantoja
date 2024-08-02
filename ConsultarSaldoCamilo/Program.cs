c7ad61ad443914d2f133c17b08e0965e98c90472
Ash20 <Ash20@CAMILO>
2024-08-01 22:08:20 -0500

    Se realiza un commit en ConsultarSaldo

---
+++ ConsultarSaldoCamilo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsultarSaldoCamilx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sea bienvenido al mejor cajero automático");
+            if (InicioSesion())
+            {
+                Console.WriteLine("Su inicio de sesión fue un éxito. ");
+                ConsultarSaldo();
+            }
+            else
+            {
+                Console.WriteLine("Su intento de inicio de sesión fue un fracaso. ");
+            }
+        }
+
+        static bool InicioSesion()
+        {
+            Console.Write("Por favor, inserte su Usuario.  ");
+            string usuario = Console.ReadLine();
+            Console.Write("Digite su clave:  ");
+            string clave = Console.ReadLine();
+            return usuario == "Camile" && clave == "1033";
+        }
+
+        static void ConsultarSaldo()
+        {
+            decimal saldo = 4000.00m; 
+            Console.WriteLine($"Su saldo actual es de: ${saldo:C}.");
+        }
+    }
+}
