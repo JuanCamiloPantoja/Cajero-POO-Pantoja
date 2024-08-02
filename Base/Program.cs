9eb1e6afba978d911bd813fd6b099889f12cd13e
Ash20 <Ash20@CAMILO>
2024-08-01 21:23:45 -0500

    Primer commit de la base.

---
+++ Base/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaseCajeroAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Un gusto, en este momento estas utilizando el mejor cajero automático existente. ");
+        }
+    }
+}
